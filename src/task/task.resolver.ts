4a0cc09870dc91406793e37578f4e80dc51443e9
ISAWASHUN <syun20000616@icloud.com>
2023-12-07 11:24:00 +0900

    databaseの処理終了

--- src/task/task.resolver.ts
+++ src/task/task.resolver.ts
@@ -1,20 +1,36 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Resolver } from '@nestjs/graphql';
 import { TaskService } from './task.service';
-import { Task } from './models/task.model';
+import { Task as TaskModel } from './models/task.model';
 import { Query } from '@nestjs/graphql';
 import { CreateTaskInput } from './dto/createTask.input';
+import { Task } from '@prisma/client';
+import { UpdateTaskInput } from './dto/updateTask.input';
 
 @Resolver()
 export class TaskResolver {
   constructor(private readonly taskService: TaskService) {}
 
-  @Query(() => [Task], { nullable: 'items' })
-  getTasks(): Task[] {
-    return this.taskService.getTasks();
+  @Query(() => [TaskModel], { nullable: 'items' })
+  async getTasks(): Promise<Task[]> {
+    return await this.taskService.getTasks();
   }
 
-  @Mutation(() => Task)
-  createTask(@Args('createTaskInput') createTaskInput: CreateTaskInput): Task {
-    return this.taskService.createTask(createTaskInput);
+  @Mutation(() => TaskModel)
+  async createTask(
+    @Args('createTaskInput') createTaskInput: CreateTaskInput,
+  ): Promise<Task> {
+    return await this.taskService.createTask(createTaskInput);
+  }
+
+  @Mutation(() => TaskModel)
+  async updateTask(
+    @Args('updateTaskInput') updateTask: UpdateTaskInput,
+  ): Promise<Task> {
+    return await this.taskService.updateTask(updateTask);
+  }
+
+  @Mutation(() => TaskModel)
+  async deleteTask(@Args('id', { type: () => Int }) id: number): Promise<Task> {
+    return await this.taskService.deleteTask(id);
   }
 }
